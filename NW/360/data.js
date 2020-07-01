c0477e807dc2482c5d713e7b2969637a661c3f92
nayeonkim <67707702+nayeonkim@users.noreply.github.com>
2020-07-02 00:07:26 +0200

    1

---
+++ NW/360/data.js
@@ -0,0 +1,179 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bubblevalley_gesamtview",
+      "name": "Bubblevalley_gesamtview",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.05001717026092933,
+        "pitch": -0.08119067586599016,
+        "fov": 1.2586780206261863
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10087346812188613,
+          "pitch": -0.29979838788860214,
+          "rotation": 3.141592653589793,
+          "target": "1-bubblevalley"
+        },
+        {
+          "yaw": -0.4036497985866081,
+          "pitch": -0.2862564419875291,
+          "rotation": 3.141592653589793,
+          "target": "2-lufthof-von-arbeitsplatz"
+        },
+        {
+          "yaw": -0.5883450365564933,
+          "pitch": -0.10632506007331344,
+          "rotation": 1.5707963267948966,
+          "target": "3-arbeitsplatz"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.08320265477700595,
+          "pitch": 0.21097218767879156,
+          "title": "Bubblevalley",
+          "text": "<p class=\"p1\" style=\"text-align: justify; font: 400 11px &quot;Maison Neue Light&quot;; color: rgb(0, 0, 0);\">Kann man den idealen Arbeitsplatz mit kulturellen Räumen verbinden?</p><p class=\"p1\" style=\"text-align: justify; font: 400 11px &quot;Maison Neue Light&quot;; color: rgb(0, 0, 0);\">‘Bubblevalley’ ist die figurale Verknüpfung zwischen Arbeit und Kultur. Die zwei unterschiedlichen Formen\\Figuren zeigen die Nutzung der jeweiligen Räume. Die Besucher können die Kultur in Bubblevalley genießen und die Arbeiter haben<span class=\"Apple-converted-space\">&nbsp;</span>einen abwechslungsreichen Ausblick.<span class=\"Apple-converted-space\">&nbsp;</span></p><p class=\"p1\" style=\"text-align: justify; font: 400 11px &quot;Maison Neue Light&quot;; color: rgb(0, 0, 0);\">Einen langweiligen Augenblick gibt’s hier nicht mehr!</p>"
+        }
+      ]
+    },
+    {
+      "id": "1-bubblevalley",
+      "name": "Bubblevalley",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07339897299428699,
+          "pitch": -0.6129711718436539,
+          "rotation": 4.71238898038469,
+          "target": "3-arbeitsplatz"
+        },
+        {
+          "yaw": -2.9395843004199946,
+          "pitch": -0.12421807425407039,
+          "rotation": 0,
+          "target": "0-bubblevalley_gesamtview"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8725699515285026,
+          "pitch": -0.35667617258373596,
+          "title": "Bubblevalley",
+          "text": "<p class=\"p1\" style=\"text-align: justify; font: 400 12px &quot;Maison Neue&quot;; color: rgb(0, 0, 0);\">Es ist ein Gebäude, in dem der Arbeitsplatz und der kulturelle Raum miteinander verbunden sind.Der Zugang des Besuchers vom kulturellen Raum ist frei und der Kontakt zwischen den Arbeitern im Büro und den Besuchern ist indirekt. Die beiden Programme (Büro und Kulturräume) unterscheiden sich in Formen/ Figuren und auch die Eingänge sind unterschiedlich gestaltet. Als kulturelle Räume gibt es ein Theater, Bibliothek und Kinos. Die kulturellen Teile des Gebäudes bestehen aus einer Bubble-Form, die jeweils unterschiedliche Höhen und Größen annehmen. Direkt nach dem Eingang zum Kulturraum wird zuerst der Bubblevalley erfahren, welcher durch die Fassade der Kulturräume geschaffen wird. Lediglich durch eine Rolltreppe in der Lobby ist es möglich, die erste Etage zu betreten. Die Bibliothek hat das größte Volumen der kulturellen Räume und ist aus zwei Richtungen zugänglich durch beide Rolltreppen. Die Erschließung erfolgt innerhalb der Bibliothek durch einen Aufzug neben dem runden Oberlicht. Zwei Kinos bieten Platz für 224 bzw. 152 Personen.</p>"
+        }
+      ]
+    },
+    {
+      "id": "2-lufthof-von-arbeitsplatz",
+      "name": "Lufthof von Arbeitsplatz",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.01247343371980314,
+          "pitch": -0.5999193733210078,
+          "rotation": 4.71238898038469,
+          "target": "1-bubblevalley"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-arbeitsplatz",
+      "name": "Arbeitsplatz",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.33347348601110127,
+          "pitch": 0.03332647467337324,
+          "rotation": 0,
+          "target": "2-lufthof-von-arbeitsplatz"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Bubblevalley",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
